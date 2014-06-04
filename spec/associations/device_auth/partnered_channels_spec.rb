5c3fc7f4add93070c81c1463fda723c21ced91d4
claudiob <claudio@fullscreen.net>
2014-06-04 15:22:30 -0700

    Faster test: don’t retrieve all partnered channels

--- spec/associations/device_auth/partnered_channels_spec.rb
+++ spec/associations/device_auth/partnered_channels_spec.rb
@@ -7,7 +7,8 @@ describe Yt::Associations::PartneredChannels, :partner do
     context 'given a content owner with partnered channels' do
       let(:content_owner) { $content_owner }
 
-      it { expect(partnered_channels.count).to be > 0 }
+      # NOTE: Uncomment once size does not runs through *all* the pages
+      # it { expect(partnered_channels.size).to be > 0 }
       it { expect(partnered_channels.first).to be_a Yt::Channel }
     end
   end
