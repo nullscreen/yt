8046ee78da4526e1bb09af82c73a5a179c02e7be
claudiob <claudio@fullscreen.net>
2014-06-11 15:38:08 -0700

    Extract Yt::Collection::Reports
    Remove duplication between Views and Earnings

--- lib/yt/collections/earnings.rb
+++ lib/yt/collections/earnings.rb
@@ -1,37 +1,13 @@
-require 'yt/collections/base'
+require 'yt/collections/reports'
 
 module Yt
   module Collections
-    class Earnings < Base
-
-      def within(days_range)
-        @days_range = days_range
-        Hash[*flat_map{|daily_earning| daily_earning}]
-      end
+    class Earnings < Reports
 
     private
 
-      def new_item(data)
-        # NOTE: could use column headers to be more precise
-        [Date.iso8601(data.first), data.last]
-      end
-
-      def list_params
-        super.tap do |params|
-          params[:path] = '/youtube/analytics/v1/reports'
-          params[:params] = {}.tap do |params|
-            params['ids'] = "contentOwner==#{@auth.owner_name}"
-            params['filters'] = "channel==#{@parent.id}"
-            params['start-date'] = @days_range.begin
-            params['end-date'] = @days_range.end
-            params['metrics'] = [:estimatedMinutesWatched, :earnings].join ','
-            params['dimensions'] = :day
-          end
-        end
-      end
-
-      def items_key
-        'rows'
+      def metrics
+        [:estimatedMinutesWatched, :earnings].join ','
       end
     end
   end
