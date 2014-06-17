1a3b93b04fec56c5cb5cc9cef3477dd38b856e57
claudiob <claudio@fullscreen.net>
2014-06-17 15:10:36 -0700

    Handle annotations of unknown or private videos
    Since there is no API endpoint, retrieving annotations of unknown
    videos or of private videos (to which YouTube responds with 403)
    should not raise an error, but simply not return any annotation.

--- spec/associations/no_auth/video_spec.rb
+++ spec/associations/no_auth/video_spec.rb
@@ -4,10 +4,17 @@ require 'yt/models/video'
 describe Yt::Video, :device_app do
   subject(:video) { Yt::Video.new id: id, auth: $account }
 
-  context 'given an existing video with annotations' do
+  context 'given a public video with annotations' do
     let(:id) { 'MESycYJytkU' }
 
     it { expect(video.annotations).to be_a Yt::Collections::Annotations }
     it { expect(video.annotations.first).to be_a Yt::Annotation }
+  end
+
+  context 'given a private video' do
+    let(:id) { 'JzDEc54FVTc' }
+
+    it { expect(video.annotations).to be_a Yt::Collections::Annotations }
+    it { expect(video.annotations.count).to be_zero }
   end
 end
