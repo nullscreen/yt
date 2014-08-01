0bab7fd98a00ce3fb1a98b453d502c6489db0c14
Jonathan Boyer <contact@grafikart.fr>
2014-08-01 19:06:42 +0200

    Added LiveStream attributes to video

---
+++ lib/yt/collections/live_streaming_details.rb
@@ -0,0 +1,31 @@
+require 'yt/collections/base'
+require 'yt/models/live_streaming_detail'
+
+module Yt
+  module Collections
+    class LiveStreamingDetails < Base
+
+    private
+
+      # @return [Yt::Models::ContentDetail] a new content detail initialized
+      #   with one of the items returned by asking YouTube for a list of them.
+      def new_item(data)
+        Yt::LiveStreamingDetail.new data: data['liveStreamingDetails']
+      end
+
+      # @return [Hash] the parameters to submit to YouTube to get the
+      #   content detail of a resource, for instance a video.
+      # @see https://developers.google.com/youtube/v3/docs/videos#resource
+      def list_params
+        super.tap do |params|
+          params[:params] = live_streaming_details_params
+          params[:path] = '/youtube/v3/videos'
+        end
+      end
+
+      def live_streaming_details_params
+        {max_results: 50, part: 'liveStreamingDetails', id: @parent.id}
+      end
+    end
+  end
+end
