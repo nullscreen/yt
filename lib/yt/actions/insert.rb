a521c9317df3c33017ae01296a1c6dbceaf70368
Sean Stavropoulos <sean.stavro@gmail.com>
2014-08-06 18:52:21 -0700

    Add references.insert to references collection

--- lib/yt/actions/insert.rb
+++ lib/yt/actions/insert.rb
@@ -7,6 +7,9 @@ module Yt
     private
 
       def do_insert(extra_insert_params = {})
+        camelize_keys! extra_insert_params[:body] if extra_insert_params[:body] && insert_params.fetch(:camelize_body, true)
+        camelize_keys! extra_insert_params[:params] if extra_insert_params[:params] && insert_params.fetch(:camelize_params, true)
+
         request = Yt::Request.new insert_params.deep_merge(extra_insert_params)
         response = request.run
         @items = []
@@ -21,12 +24,21 @@ module Yt
           params[:method] = :post
           params[:auth] = @auth
           params[:expected_response] = Net::HTTPOK
+          params[:camelize_params] = true
+          params[:camelize_body] = true
         end
       end
 
       def extract_data_from(response)
         response.body
       end
+
+      def camelize_keys!(hash = {})
+        hash.dup.each_key do |key|
+          hash[key.to_s.camelize(:lower).to_sym] = hash.delete key
+        end
+      end
+
     end
   end
 end
