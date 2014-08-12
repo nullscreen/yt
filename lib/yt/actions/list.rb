nullscreen/yt

commit 4c192bed332f2ec7542e9ed5c1da654bbdd27999

`size` doesn’t load all the pages of collection
Calling `size` on a collection (for instance on account.videos to know the
number of videos owned by an account) will now return the number of items as
specified in the "totalResults" field of the first page of the YouTube
response, rather than loading *all* the pages (possibly thousands) and counting
exactly how many items are returned.

The old behavior is kept the same for the `count` method.

lib/yt/actions/list.rb
@@ -4,8 +4,8 @@ require 'yt/errors/no_items'
 module Yt
   module Actions
     module List
-      delegate :count, :first, :any?, :each, :map, :flat_map, :find, to: :list
-      alias size count
+      delegate :count, :first, :any?, :each, :map, :flat_map, :find,
+        :size, to: :list
 
       def first!
         first.tap{|item| raise Errors::NoItems, last_request unless item}
@@ -15,12 +15,21 @@ module Yt
 
       def list
         @last_index, @page_token = 0, nil
-        Enumerator.new do |items|
+        Enumerator.new(-> {total_results}) do |items|
           while next_item = find_next
             items << next_item
           end
           @where_params = {}
         end
+      end
+
+      # @private
+      # Returns the total number of items that YouTube can provide for the
+      # given request, either all in one page or in consecutive pages.
+      def total_results
+        response = request(list_params).run
+        total_results = response.body.fetch('pageInfo', {})['totalResults']
+        total_results ||= response.body.fetch(items_key, []).size
       end
 
       def find_next

lib/yt/version.rb
@@ -1,3 +1,3 @@
 module Yt
-  VERSION = '0.9.8'
+  VERSION = '0.10.0'
 end

spec/collections/videos_spec.rb
@@ -6,9 +6,22 @@ describe Yt::Collections::Videos do
   subject(:collection) { Yt::Collections::Videos.new parent: channel }
   let(:channel) { Yt::Channel.new id: 'any-id' }
   let(:page) { {items: [], token: 'any-token'} }
-  let(:query) { {q: 'search string'} }
+
+  describe '#size' do
+    describe 'sends only one request and return the total results' do
+      let(:total_results) { 123456 }
+      before do
+        expect_any_instance_of(Yt::Request).to receive(:run).once do
+          double(body: {'pageInfo'=>{'totalResults'=>total_results}})
+        end
+      end
+      it { expect(collection.size).to be total_results }
+    end
+  end
 
   describe '#count' do
+    let(:query) { {q: 'search string'} }
+
     context 'called once with .where(query) and once without' do
       after do
         collection.where(query).count

spec/requests/as_content_owner/content_owner_spec.rb
@@ -3,8 +3,7 @@ require 'yt/models/content_owner'
 
 describe Yt::ContentOwner, :partner do
   describe '.partnered_channels' do
-    # NOTE: Uncomment once size does not runs through *all* the pages
-    # it { expect($content_owner.partnered_channels.size).to be > 0 }
+    it { expect($content_owner.partnered_channels.size).to be > 0 }
     it { expect($content_owner.partnered_channels.first).to be_a Yt::Channel }
   end
 

lib/yt/collections/annotations.rb
@@ -46,6 +46,15 @@ module Yt
         expected?(error) ? [] : raise(error)
       end
 
+      # @private
+      # @note Annotations overwrites +total_results+ since `items_key` is
+      #   not `items` for annotations.
+      # @todo Remove this function by extracting the keys used by annotations
+      #   in a method and reusing them both in `next_page` and `total_results`.
+      def total_results
+        count
+      end
+
       # Since there is no API endpoint, retrieving annotations of unknown
       # videos or of private videos (to which YouTube responds with 403)
       # should not raise an error, but simply not return any annotation.

spec/requests/as_account/channel_spec.rb
@@ -42,12 +42,15 @@ describe Yt::Channel, :device_app do
       it { expect(channel.unsubscribe!).to be_truthy }
     end
 
-    # NOTE: These tests are slow because they go through multiple pages of
-    # results and do so to test that we can overcome YouTube’s limitation of
-    # only returning the first 500 results for each query.
+    # @note: these tests are slow because they go through multiple pages of
+    #   results and do so to test that we can overcome YouTube’s limitation of
+    #   only returning the first 500 results for each query.
+    # @note: in principle, the following three counters should match, but in
+    #   reality +video_count+ and +size+ are only approximations.
     context 'with more than 500 videos' do
       let(:id) { 'UCsmvakQZlvGsyjyOhmhvOsw' }
       it { expect(channel.video_count).to be > 500 }
+      it { expect(channel.videos.size).to be > 500 }
       it { expect(channel.videos.count).to be > 500 }
     end
   end
@@ -126,7 +129,8 @@ describe Yt::Channel, :device_app do
       # NOTE: This test is just a reflection of YouTube irrational behavior of
       # returns 0 results if the name of an unknown channel starts with UC, but
       # returning 100,000 results otherwise (ignoring the channel filter).
-      it { expect(channel.videos.count).to be 0 }
+      it { expect(channel.videos.count).to be_zero }
+      it { expect(channel.videos.size).to be_zero }
     end
   end
 end

spec/requests/as_server_app/channel_spec.rb
@@ -36,7 +36,8 @@ describe Yt::Channel, :server_app do
       # NOTE: This test is just a reflection of YouTube irrational behavior of
       # returns 0 results if the name of an unknown channel starts with UC, but
       # returning 100,000 results otherwise (ignoring the channel filter).
-      it { expect(channel.videos.count).to be 0 }
+      it { expect(channel.videos.count).to be_zero }
+      it { expect(channel.videos.size).to be_zero }
     end
   end
 end

spec/requests/unauthenticated/video_spec.rb
@@ -9,6 +9,7 @@ describe Yt::Video do
 
     it { expect(video.annotations).to be_a Yt::Collections::Annotations }
     it { expect(video.annotations.first).to be_a Yt::Annotation }
+    it { expect(video.annotations.size).to be > 0 }
   end
 
   context 'given a private video' do
@@ -16,5 +17,6 @@ describe Yt::Video do
 
     it { expect(video.annotations).to be_a Yt::Collections::Annotations }
     it { expect(video.annotations.count).to be_zero }
+    it { expect(video.annotations.size).to be_zero }
   end
 end

commit 91486eac056cffcf5e459b22dd800b99105d1f51

FIX support for Ruby 1.9 broken by 4c192be
In Ruby 1.9, Enumerator.new does not take an optional 'size' parameter.
To make the code compatible with Ruby 1.9 (again), a new Yt::Iterator
class is added that inherits from Enumerator but is compatible with Ruby 1.9.

lib/yt/actions/list.rb
@@ -1,4 +1,5 @@
 require 'yt/models/request'
+require 'yt/models/iterator'
 require 'yt/errors/no_items'
 
 module Yt
@@ -15,7 +16,7 @@ module Yt
 
       def list
         @last_index, @page_token = 0, nil
-        Enumerator.new(-> {total_results}) do |items|
+        Yt::Iterator.new(-> {total_results}) do |items|
           while next_item = find_next
             items << next_item
           end

lib/yt/models/iterator.rb
@@ -1,0 +1,15 @@
+module Yt
+  module Models
+    # If we dropped support for Ruby 1.9.3, then we could simply use Enumerator
+    # which takes a `size` parameter in Ruby >= 2.
+    class Iterator < Enumerator
+      def initialize(size=nil, &block)
+        RUBY_VERSION < '2' ? super(&block) : super(size, &block)
+      end
+
+      def size
+        RUBY_VERSION < '2' ? count : super
+      end
+    end
+  end
+end

lib/yt/version.rb
@@ -1,3 +1,3 @@
 module Yt
-  VERSION = '0.10.0'
+  VERSION = '0.10.1'
 end

spec/collections/videos_spec.rb
@@ -7,7 +7,7 @@ describe Yt::Collections::Videos do
   let(:channel) { Yt::Channel.new id: 'any-id' }
   let(:page) { {items: [], token: 'any-token'} }
 
-  describe '#size' do
+  describe '#size', :ruby2 do
     describe 'sends only one request and return the total results' do
       let(:total_results) { 123456 }
       before do

spec/requests/as_content_owner/content_owner_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 require 'yt/models/content_owner'
 
 describe Yt::ContentOwner, :partner do
-  describe '.partnered_channels' do
-    it { expect($content_owner.partnered_channels.size).to be > 0 }
+  describe '.partnered_channels.first' do
     it { expect($content_owner.partnered_channels.first).to be_a Yt::Channel }
+  end
+
+  describe '.partnered_channels.size', :ruby2 do
+    it { expect($content_owner.partnered_channels.size).to be > 0 }
   end
 
   describe '.claims' do

spec/spec_helper.rb
@@ -11,4 +11,8 @@ Dir['./spec/support/**/*.rb'].each {|f| require f}
 
 RSpec.configure do |config|
   config.order = 'random'
+  config.run_all_when_everything_filtered = false
+  # @note: Some tests might take too long to run on Ruby 1.9.3 which does not
+  #   support "size" for Enumerator, so we are better off skipping them.
+  config.filter_run_excluding ruby2: true if RUBY_VERSION < '2'
 end