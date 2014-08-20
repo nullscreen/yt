d7f7bc92b529b4c3fd3473a868be8e12e73a3033
claudiofullscreen <claudio@fullscreen.net>
2014-08-20 08:45:16 -0700

    Fix Yt::Collection in documentation
    It’s supposed to be Yt::Collections instead

--- lib/yt/models/content_owner.rb
+++ lib/yt/models/content_owner.rb
@@ -8,19 +8,19 @@ module Yt
     class ContentOwner < Account
 
       # @!attribute [r] partnered_channels
-      #   @return [Yt::Collection::PartneredChannels] the channels managed by the CMS account.
+      #   @return [Yt::Collections::PartneredChannels] the channels managed by the CMS account.
       has_many :partnered_channels
 
       # @!attribute [r] claims
-      #   @return [Yt::Collection::Claims] the claims administered by the content owner.
+      #   @return [Yt::Collections::Claims] the claims administered by the content owner.
       has_many :claims
 
       # @!attribute [r] references
-      #   @return [Yt::Collection::References] the references administered by the content owner.
+      #   @return [Yt::Collections::References] the references administered by the content owner.
       has_many :references
 
       # @!attribute [r] policies
-      #   @return [Yt::Collection::Policies] the policies saved by the content owner.
+      #   @return [Yt::Collections::Policies] the policies saved by the content owner.
       has_many :policies
 
       def initialize(options = {})
