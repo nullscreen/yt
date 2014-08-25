a5a1b3a5b544ba1a1cf55e18b2cfa0763fb9ea70
claudiofullscreen <claudio@fullscreen.net>
2014-08-25 12:04:24 -0700

    Extract duplicate `sanitize_brackets!`

--- lib/yt/collections/resources.rb
+++ lib/yt/collections/resources.rb
@@ -59,17 +59,6 @@ module Yt
         (part[:keys] & attributes.keys).any?
       end
 
-      # @return [Hash] the original hash with angle brackets characters in its
-      #   values replaced with similar Unicode characters accepted by Youtube.
-      # @see https://support.google.com/youtube/answer/57404?hl=en
-      def sanitize_brackets!(source)
-        case source
-          when String then source.gsub('<', '‹').gsub('>', '›')
-          when Array then source.map{|string| sanitize_brackets! string}
-          when Hash then source.each{|k,v| source[k] = sanitize_brackets! v}
-        end
-      end
-
       def camelize(value)
         value.to_s.camelize(:lower).to_sym
       end
