nullscreen/yt

commit 2ee1a6994a163abbc3366ed15934a04342828209

Extend snippet to include all the YouTube methods
Now resources (Channel, Video, Playlist, PlaylistItem) have access
to all the data made available by YouTube in the 'snippet' part.
All the following methods are now available:

video.channel_id #=> "UCxO1tY8h1AhOz0T4ENwmpow"
video.channel_title #=> "Fullscreen"
video.category_id #=> "22"
video.live_broadcast_content #=> "none"
playlist.tags #=> []
playlist.channel_id #=> "UCxO1tY8h1AhOz0T4ENwmpow"
playlist.channel_title #=> "Fullscreen"
item.channel_id #=> "UCxO1tY8h1AhOz0T4ENwmpow"
item.channel_title #=> "Fullscreen"
item.playlist_id #=> "PLSWYkYzOrPMRCK6j0UgryI8E0NHhoVdRc"
item.video_id #=> "W4GhTprSsOY"

As part of this commit, PlaylistItem now inherit from Resource, and
therefore also has access to the status methods:

item.public? #=> true

lib/yt/collections/snippets.rb
@@ -10,7 +10,7 @@ module Yt
       # @return [Yt::Models::Snippet] a new snippet initialized with
       #   one of the items returned by asking YouTube for a list of snippets.
       def new_item(data)
-        Yt::Snippet.new data: data['snippet']
+        Yt::Snippet.new data: data['snippet'], auth: @auth
       end
 
       # @return [Hash] the parameters to submit to YouTube to get the
@@ -19,8 +19,14 @@ module Yt
       def list_params
         super.tap do |params|
           params[:params] = {id: @parent.id, part: 'snippet'}
-          params[:path] = "/youtube/v3/#{@parent.kind.pluralize}"
         end
+      end
+
+      # @private
+      # @note Snippets overrides +list_resources+ since the endpoint is not 
+      #   '/snippets' but the endpoint related to the snippet’s resource.
+      def list_resources
+        @parent.class.to_s.pluralize
       end
     end
   end

lib/yt/collections/statuses.rb
@@ -21,8 +21,14 @@ module Yt
       def list_params
         super.tap do |params|
           params[:params] = {id: @parent.id, part: 'status'}
-          params[:path] = "/youtube/v3/#{@parent.kind.pluralize}"
         end
+      end
+
+      # @private
+      # @note Statuses overrides +list_resources+ since the endpoint is not 
+      #   '/statuses' but the endpoint related to the snippet’s resource.
+      def list_resources
+        @parent.class.to_s.pluralize
       end
     end
   end

lib/yt/version.rb
@@ -1,3 +1,3 @@
 module Yt
-  VERSION = '0.7.0'
+  VERSION = '0.7.1'
 end

lib/yt/models/account.rb
@@ -16,9 +16,8 @@ module Yt
       # @!attribute [r] user_info
       #   @return [Yt::Models::UserInfo] the account’s profile information.
       has_one :user_info
-      delegate :id, :email, :has_verified_email?, :gender, :name,
-               :given_name, :family_name, :profile_url, :avatar_url,
-               :locale, :hd, to: :user_info
+      delegate :id, :email, :has_verified_email?, :gender, :name, :given_name,
+        :family_name, :profile_url, :avatar_url, :locale, :hd, to: :user_info
 
       # @!attribute [r] videos
       #   @return [Yt::Collections::Videos] the videos owned by the account.

lib/yt/models/channel.rb
@@ -22,12 +22,11 @@ module Yt
       #   @return [Yt::Collections::Playlists] the channel’s playlists.
       has_many :playlists
 
-
       # @!attribute [r] statistics_set
       #   @return [Yt::Models::StatisticsSet] the statistics for the video.
       has_one :statistics_set
       delegate :view_count, :comment_count, :video_count, :subscriber_count,
-               :subscriber_count_visible?, to: :statistics_set
+        :subscriber_count_visible?, to: :statistics_set
 
       # Returns whether the authenticated account is subscribed to the channel.
       #

lib/yt/models/playlist.rb
@@ -5,6 +5,7 @@ module Yt
     # Provides methods to interact with YouTube playlists.
     # @see https://developers.google.com/youtube/v3/docs/playlists
     class Playlist < Resource
+      delegate :tags, :channel_id, :channel_title, to: :snippet
 
       # @!attribute [r] playlist_items
       #   @return [Yt::Collections::PlaylistItems] the playlist’s items.

lib/yt/models/playlist_item.rb
@@ -1,28 +1,12 @@
-require 'yt/models/base'
+require 'yt/models/resource'
 
 module Yt
   module Models
     # Provides methods to interact with YouTube playlist items.
     # @see https://developers.google.com/youtube/v3/docs/playlistItems
-    class PlaylistItem < Base
-      # @return [String] the ID that uniquely identify a YouTube playlist item.
-      attr_reader :id
-
-      # @return [String] the order in which the item appears in the playlist.
-      #   The value uses a zero-based index, so the first item has a position
-      #   of 0, the second item has a position of 1, and so forth.
-      attr_reader :position
-
-      attr_reader :video
-
-      def initialize(options = {})
-        @id = options[:id]
-        @auth = options[:auth]
-        if options[:snippet]
-          @position = options[:snippet]['position']
-          @video = Video.new video_params_for options
-        end
-      end
+    class PlaylistItem < Resource
+      delegate :channel_id, :channel_title, :playlist_id, :position, :video_id,
+        :video, to: :snippet
 
       def delete(options = {})
         do_delete {@id = nil}
@@ -41,14 +25,6 @@ module Yt
         super.tap do |params|
           params[:path] = '/youtube/v3/playlistItems'
           params[:params] = {id: @id}
-        end
-      end
-
-      def video_params_for(options = {})
-        {}.tap do |params|
-          params[:id] = options[:snippet].fetch('resourceId', {})['videoId']
-          params[:snippet] = options[:snippet].except 'resourceId'
-          params[:auth] = options[:auth]
         end
       end
     end

lib/yt/models/resource.rb
@@ -9,7 +9,7 @@ module Yt
 
       has_one :snippet
       delegate :title, :description, :thumbnail_url, :published_at,
-               :tags, to: :snippet
+        to: :snippet
 
       has_one :status
       delegate :privacy_status, :public?, :private?, :unlisted?, to: :status

commit ad9a6ae9df181cca112cb621d75a4e6780ad0041

FIX Retrieve data for resources by URL
The behavior was broken by 2ee1a69 since that commit changed the way
in which the endpoint was selected: instead of hitting /channels or
/videos, the endpoint was changed to /resources (which does not exist).

lib/yt/collections/snippets.rb
@@ -11,17 +11,11 @@ module Yt
         {data: data['snippet'], auth: @auth}
       end
 
-      # # @return [Yt::Models::Snippet] a new snippet initialized with
-      # #   one of the items returned by asking YouTube for a list of snippets.
-      # def new_item(data)
-      #   Yt::Snippet.new data: data['snippet'], auth: @auth
-      # end
-
       # @return [Hash] the parameters to submit to YouTube to get the
       #   snippet of a resource, for instance a channel.
       # @see https://developers.google.com/youtube/v3/docs/channels#resource
       def list_params
-        endpoint = @parent.class.to_s.pluralize.demodulize.camelize :lower
+        endpoint = @parent.kind.pluralize.camelize :lower
         super.tap do |params|
           params[:path] = "/youtube/v3/#{endpoint}"
           params[:params] = {id: @parent.id, part: 'snippet'}

lib/yt/collections/statuses.rb
@@ -15,7 +15,7 @@ module Yt
       #   of a resource, for instance a channel.
       # @see https://developers.google.com/youtube/v3/docs/channels/list
       def list_params
-        endpoint = @parent.class.to_s.pluralize.demodulize.camelize :lower
+        endpoint = @parent.kind.pluralize.camelize :lower
         super.tap do |params|
           params[:path] = "/youtube/v3/#{endpoint}"
           params[:params] = {id: @parent.id, part: 'status'}

lib/yt/version.rb
@@ -1,3 +1,3 @@
 module Yt
-  VERSION = '0.11.5'
+  VERSION = '0.11.6'
 end

spec/requests/as_server_app/resource_spec.rb
@@ -4,15 +4,43 @@ require 'yt/models/resource'
 describe Yt::Resource, :server_app do
   subject(:resource) { Yt::Resource.new url: url }
 
-  describe '#id' do
-    context 'given a URL containing an existing username' do
-      let(:url) { 'youtube.com/fullscreen' }
-      it { expect(resource.id).to eq 'UCxO1tY8h1AhOz0T4ENwmpow' }
-    end
+  context 'given the URL of an existing channel' do
+    let(:url) { 'youtube.com/fullscreen' }
 
-    context 'given a URL containing an unknown username' do
-      let(:url) { 'youtube.com/--not--a--valid--username' }
-      it { expect{resource.id}.to raise_error Yt::Errors::NoItems }
+    specify 'provides access to its data' do
+      expect(resource.id).to eq 'UCxO1tY8h1AhOz0T4ENwmpow'
+      expect(resource.title).to eq 'Fullscreen'
+      expect(resource.privacy_status).to eq 'public'
+    end
+  end
+
+  context 'given the URL of an existing video' do
+    let(:url) { 'https://www.youtube.com/watch?v=MESycYJytkU&list=LLxO1tY8h1AhOz0T4ENwmpow' }
+
+    specify 'provides access to its data' do
+      expect(resource.id).to eq 'MESycYJytkU'
+      expect(resource.title).to eq 'Fullscreen Creator Platform'
+      expect(resource.privacy_status).to eq 'public'
+    end
+  end
+
+  context 'given the URL of an existing playlist' do
+    let(:url) { 'https://www.youtube.com/playlist?list=LLxO1tY8h1AhOz0T4ENwmpow' }
+
+    specify 'provides access to its data' do
+      expect(resource.id).to eq 'LLxO1tY8h1AhOz0T4ENwmpow'
+      expect(resource.title).to eq 'Liked videos'
+      expect(resource.privacy_status).to eq 'public'
+    end
+  end
+
+  context 'given an unknown URL' do
+    let(:url) { 'youtube.com/--not-a-valid-url--' }
+
+    specify 'accessing its data raises an error' do
+      expect{resource.id}.to raise_error Yt::Errors::NoItems
+      expect{resource.title}.to raise_error Yt::Errors::NoItems
+      expect{resource.status}.to raise_error Yt::Errors::NoItems
     end
   end
 end