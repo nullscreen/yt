a1c1b47ee823b4059188a4690b6611f54705d6ec
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-06-09 12:33:24 -0700

    Use prompt=consent

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,12 @@ ENV['YT_TEST_CLIENT_SECRET'] ||= 'YYY'
 ENV['YT_TEST_API_KEY'] ||= 'ZZZ'
 ENV['YT_TEST_REFRESH_TOKEN'] ||= 'ABC'
 
+ENV['YT_TEST_CONTENT_OWNER_NAME'] ||= 'abcd'
+ENV['YT_TEST_PARTNER_CLIENT_ID'] ||= 'abcd'
+ENV['YT_TEST_PARTNER_CLIENT_SECRET'] ||= 'abcd'
+ENV['YT_TEST_CONTENT_OWNER_REFRESH_TOKEN'] ||= 'abcd'
+ENV['YT_TEST_CONTENT_OWNER_ACCESS_TOKEN'] ||= 'abcd'
+
 Dir['./spec/support/**/*.rb'].each {|f| require f}
 
 RSpec.configure do |config|
